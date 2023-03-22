8ae3a72d1a177775be423f5e7baeab4bc578973a
Sam Thompson <thompsonsed@gmail.com>
2023-03-22 14:53:57 +0000

    Fixes layout for updated qmk versions

--- keyboards/dztech/dz65rgb/keymaps/thompsonsed/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/thompsonsed/keymap.c
@@ -23,7 +23,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi(
-        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
+        QK_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_NUBS, KC_PGUP,
         MO(1),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
@@ -31,15 +31,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 	[1] = LAYOUT_65_ansi(
         KC_GRAVE,        KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,    KC_HOME,
-        KC_TAB,         _______, _______, _______, KC_WREF, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_SLCK, KC_PAUS, KC_GRAVE,  KC_MEDIA_PLAY_PAUSE,
-        MO(0), 			_______, _______, KC_WBAK, KC_WFWD, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_NUHS,          EEP_RST,   KC_PSCREEN,
+        KC_TAB,         _______, _______, _______, KC_WREF, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PSCR, KC_SCRL, KC_PAUS, KC_GRAVE,  KC_MEDIA_PLAY_PAUSE,
+        MO(0), 			_______, _______, KC_WBAK, KC_WFWD, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_NUHS,          EE_CLR,   KC_PSCR,
         KC_LSFT,                 KC_NUHS, _______, _______, _______, _______, NK_TOGG, _______, ARRW_L,  ARRW_R,  _______, _______, KC_VOLU,   KC_MUTE,
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    DF(0),   _______, MO(0),   KC_MPRV, KC_VOLD,   KC_MNXT
     ),
     [2] = LAYOUT_65_ansi(
         KC_GRAVE,        KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,    KC_HOME,
-        _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_GRAVE,  KC_MEDIA_PLAY_PAUSE,
-        MO(1), 			RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, KC_NUHS,          EEP_RST,   KC_PSCREEN,
+        _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_GRAVE,  KC_MEDIA_PLAY_PAUSE,
+        MO(1), 			RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, KC_NUHS,          EE_CLR,   KC_PSCR,
         KC_LSFT,                 KC_NUHS, _______, _______, _______, _______, NK_TOGG, _______, ARRW_L,  ARRW_R,  _______, _______, KC_VOLU,   KC_MUTE,
         _______,        _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD,   KC_MNXT
     )
